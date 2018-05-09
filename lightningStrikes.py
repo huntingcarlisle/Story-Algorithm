a5c3d7c869fa74232438b82c629ca5504ee4a3cb
Huntingcarlisle <huntingcarlisle@gmail.com>
2018-05-09 06:01:37 +0000

    add Question One

---
+++ lightningStrikes.py
@@ -0,0 +1,22 @@
+def lightningStrike():
+    print("===========")
+    print("Welcome to the story algorithm!")
+    print()
+
+    # Question One
+    lightning = input("Lightning has struck---did you cause it (A) or was it outside of your control (B)?: ")
+    print()
+    if lightning == "A":
+        print("Your story is either Revenge, Escape, Save the Village, Mystery, Chasing the Dream, Redemption, or Unintended")
+        print()
+    elif lightning == "B":
+        print("Your story is either Chasing the Dream, Redemption, or Unintended.")
+        print()
+    else:
+        print("Invalid Selection")
+        print()
+
+    # Question Two
+
+
+lightningStrike()
